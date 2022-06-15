78a953834397059363650400c7dbc827c7f61884
Sergey Bednyakov <sbednyakov@gmail.com>
2022-06-15 14:55:23 +0300

    master: initial commit

---
+++ DictionaryBot/BotCredentials.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegramBot
+{
+    public static class BotCredentials
+    {
+        public static readonly string BotToken = "5572344355:AAFm-RUgYjqo7HEf723LnjVSqejkqFvdjQw"; 
+    }
+}
