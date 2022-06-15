144f033d9d25ec13a24342b2436e0e5a2480f8e7
Sergey Bednyakov <sbednyakov@gmail.com>
2022-06-15 14:55:23 +0300

    master:  add /dictionary command

---
+++ DictionaryBot/Commands/ChatTextCommandOption.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegramBot.Commands
+{
+    public abstract class ChatTextCommandOption : AbstractCommand
+    {
+        public override bool CheckMessage(string message)
+        {
+            return message.StartsWith(CommandText);
+        }
+
+        public string ClearMessageFromCommand(string message)
+        {
+            string word = default;
+            try
+            {
+                word = message.Substring(CommandText.Length + 1);
+            }
+            catch(ArgumentOutOfRangeException)
+            {
+                return word;
+            }
+            return message.Substring(CommandText.Length + 1);
+        }
+
+    }
+}
---
+++ DictionaryBot/Commands/ShowDictionary.cs
@@ -0,0 +1,38 @@
+﻿using TelegramBot.EnglishTrainer.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace TelegramBot.Commands
+{
+    public class ShowDictionary : AbstractCommand, IChatTextCommandWithAction
+    {
+        private ITelegramBotClient botClient;
+        private Dictionary<string, Word> dictionary;
+
+        public ShowDictionary(ITelegramBotClient botClient)
+        {
+            CommandText = "/dictionary";
+
+            this.botClient = botClient;
+        }
+
+        public bool DoAction(Conversation chat)
+        {
+            foreach(var record in chat.dictionary)
+            {
+                dictionary = chat.dictionary;
+                botClient.SendTextMessageAsync(chat.GetId(), string.Concat(record.Value.Russian, " <==> ", record.Value.English));
+            };
+            return !chat.IsAddingInProcess;
+
+        }
+
+        public string ReturnText()
+        {
+            return $"В словарь введено {dictionary?.Count ?? 0} пар слов";
+        }
+    }
+}
